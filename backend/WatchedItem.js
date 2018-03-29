520bd76331efd8775a15fe6aab9b3ee55fa3f855
jmb12686 <jmb186@gmail.com>
2018-03-29 13:45:51 -0400

    Added logic to watcher job to fetch latest price of watched items.

---
+++ backend/WatchedItem.js
@@ -0,0 +1,20 @@
+
+
+/*
+{
+    "category": "Health & Personal Care",
+    "initialPrice": 51.47,
+    "priceThreshold": 48,
+    "url": "https://www.amazon.com/dp/B00ZV2DQC6/",
+    "name": "Pampers Swaddlers Disposable Diapers Size 5, 152 Count, ONE MONTH SUPPLY"
+    "latestDetails": {
+        "latestPrice" : X,
+        "timestamp" : X,
+    }
+
+
+
+}
+
+
+*/
--- backend/watcher.js
+++ backend/watcher.js
@@ -1,13 +1,13 @@
 'use strict';
+// const async = require("async");
 const AWS = require('aws-sdk');
+const scraper = require("./scraper");
 const WATCHED_ITEMS_TABLE = process.env.WATCHED_ITEMS_TABLE; // This is the variable we setup in the serverless.yml.  Lambda env vars are in process.env
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 
 module.exports.run = (event, context, callback) => {
-  //TODO:
-  //1st - query dynamo WATCHED_ITEMS_TABLE
-  //2nd - for each, fire off request to get latest price + update latest price + send notification if < threshold
+
   console.log("***** Executing watcher job.  Scanning Dynamo table.....");
   var params = {
     TableName: WATCHED_ITEMS_TABLE,
@@ -21,8 +21,38 @@ module.exports.run = (event, context, callback) => {
     } else {
       console.log("Scan succeeded.");
       data.Items.forEach(function(item) {
-        console.log("TODO: Get latest price for item: "+JSON.stringify(item));
-        console.log("TODO: IF latest price < threshold, send notification!");
+        console.log("Getting latest price for item: "+JSON.stringify(item));
+        scraper.getPrice(item.url, function(price) {
+            console.log("latest price for item @ url "+ item.url+ " : "+price);
+            var latestDetails = {
+              latestPrice: price,
+              timestamp: new Date().toISOString(),
+            };
+            // Update record in DynamoDB
+            var updateParams = {
+              TableName: WATCHED_ITEMS_TABLE,
+              Key: {
+                url: item.url
+              },
+              UpdateExpression: "set latestDetails = :latestDetails",
+                ExpressionAttributeValues:{
+                        ":latestDetails":latestDetails
+                    },
+                ReturnValues:"UPDATED_NEW"
+            };
+            dynamoDb.update(updateParams, function(err, data) {
+                if (err) {
+                    console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+                }
+            });
+
+        });
+        //item.latestDetails[latestPrice]=x;
+        //item.latestDetails[timestamp]=y;
+
+
       });
       console.log("Done scanning, returning now...");
       callback(null, "Success");
